8d31ec4c35e52972f213fbb7f22a101a078d4118
rohanbindelowar <145816933+rohanbindelowar@users.noreply.github.com>
2024-03-22 13:46:51 -0400

    used outside click on this

--- src/components/use-Fetch/TestUseFetch.jsx
+++ src/components/use-Fetch/TestUseFetch.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import useFetch from "./useFetch";
+import useOutSideClick from "../use-outside-click/OutsideClick"
 
 export const TestUseFetch = () => {
+  
   const { data, error, loading } = useFetch("https://dummyjson.com/users", {});
   const [showData, setShowData] = useState(false);
-  console.log(data, error, loading);
+  const ref = useRef();
+  useOutSideClick(ref, ()=>setShowData(false));
 
   function handleOnClick(){
     setShowData(!showData);
@@ -14,17 +17,19 @@ export const TestUseFetch = () => {
       style={{ display: "flex", flexDirection: "column", alignItems: "center" ,gap: "10px"}}
     >
       <h1>UseFetch Hook</h1>
-      <div>
+      <div  >
         {loading ? <h1>Loading data!!!!!!</h1> : null}
         {error ? <h3>{error}</h3> : null}
         <button onClick={handleOnClick}>Users Name</button>
-        {
+       <div ref={ref}>
+       {
           showData?
             data && data.users && data.users.length
               ? data.users.map((user) => <p key={user.key}>{user.firstName}</p>)
               : null
           :null
         }
+       </div>
       </div>
     </div>
   );
